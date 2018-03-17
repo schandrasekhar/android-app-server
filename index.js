6039312bcbc17ae17eb96e39b8f4b47e4a448619
Chandra sekhar <chandrasekhar971@gmail.com>
2018-03-16 19:21:29 -0700

    updated url

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var port = 5000;
 
 var mainContent = [{
     id: '41db2ccc-944a-31a4-90e1-06b62cde7f46',
-    imageUrl: 'https://media.zenfs.com/en_US/News/TheWrap/Oprah_Would_Beat_Donald_Trump-b3cb921d9d68a116e0605084bba26f97',
+    imageUrl: 'test url',
     event: 'TRUMP ON TRAVEL BAN',
     provider: 'AOL.com',
     publishedDate: 3123123123123,
@@ -21,7 +21,7 @@ var mainContent = [{
 
 var timelineContent = [{
     id: '41db2ccc-944a-31a4-90e1-06b62cde7f46',
-    imageUrl: 'https://media.zenfs.com/en_US/News/TheWrap/Oprah_Would_Beat_Donald_Trump-b3cb921d9d68a116e0605084bba26f97',
+    imageUrl: 'test url',
     event: 'TRUMP ON TRAVEL BAN',
     provider: 'AOL.com',
     publishedDate: 3123123123123,
@@ -34,7 +34,7 @@ var timelineContent = [{
           'Florida but not Virginia. Kaine asked: "Is it because the governor of Florida is a Republican and the Virginia governor is a Democrat?'
 }, {
     id: '41db2ccc-944a-31a4-90e1-06b62cde7f46',
-    imageUrl: 'https://media.zenfs.com/en_US/News/TheWrap/Oprah_Would_Beat_Donald_Trump-b3cb921d9d68a116e0605084bba26f97',
+    imageUrl: 'test url',
     event: 'TRUMP ON TRAVEL BAN',
     provider: 'AOL.com',
     publishedDate: 3123123123123,
@@ -47,7 +47,7 @@ var timelineContent = [{
           'Florida but not Virginia. Kaine asked: "Is it because the governor of Florida is a Republican and the Virginia governor is a Democrat?'
 }, {
     id: '41db2ccc-944a-31a4-90e1-06b62cde7f46',
-    imageUrl: 'https://media.zenfs.com/en_US/News/TheWrap/Oprah_Would_Beat_Donald_Trump-b3cb921d9d68a116e0605084bba26f97',
+    imageUrl: 'test url',
     event: 'TRUMP ON TRAVEL BAN',
     provider: 'AOL.com',
     publishedDate: 3123123123123,
@@ -60,33 +60,7 @@ var timelineContent = [{
           'Florida but not Virginia. Kaine asked: "Is it because the governor of Florida is a Republican and the Virginia governor is a Democrat?'
 }, {
     id: '41db2ccc-944a-31a4-90e1-06b62cde7f46',
-    imageUrl: 'https://media.zenfs.com/en_US/News/TheWrap/Oprah_Would_Beat_Donald_Trump-b3cb921d9d68a116e0605084bba26f97',
-    event: 'TRUMP ON TRAVEL BAN',
-    provider: 'AOL.com',
-    publishedDate: 3123123123123,
-    views: 123123,
-    headline: 'Trump Denies Collusion But Doesn\'t Commit to Mueller Interview',
-    body: 'Democrats from coastal states are accusing the Trump administration of punishing states with Democratic leaders after the administration said it would block oil drilling off ' + 
-          'Florida\'s coast following objections from that state\'s Republican governor. Democratic Rep. Adam Schiff of California said on Twitter that his state, "like Florida, ' + 
-          'has hundreds of miles of beautiful coastline and a governor who wants to keep it that way. Or is that not enough for blue states? Democratic Sen. ' + 
-          'Tim Kaine of Virginia said Interior Secretary Ryan Zinke was being hypocritical in claiming that "local voices matter" in his decision to block offshore drilling in ' + 
-          'Florida but not Virginia. Kaine asked: "Is it because the governor of Florida is a Republican and the Virginia governor is a Democrat?'
-}, {
-    id: '41db2ccc-944a-31a4-90e1-06b62cde7f46',
-    imageUrl: 'https://media.zenfs.com/en_US/News/TheWrap/Oprah_Would_Beat_Donald_Trump-b3cb921d9d68a116e0605084bba26f97',
-    event: 'TRUMP ON TRAVEL BAN',
-    provider: 'AOL.com',
-    publishedDate: 3123123123123,
-    views: 123123,
-    headline: 'Trump Denies Collusion But Doesn\'t Commit to Mueller Interview',
-    body: 'Democrats from coastal states are accusing the Trump administration of punishing states with Democratic leaders after the administration said it would block oil drilling off ' + 
-          'Florida\'s coast following objections from that state\'s Republican governor. Democratic Rep. Adam Schiff of California said on Twitter that his state, "like Florida, ' + 
-          'has hundreds of miles of beautiful coastline and a governor who wants to keep it that way. Or is that not enough for blue states? Democratic Sen. ' + 
-          'Tim Kaine of Virginia said Interior Secretary Ryan Zinke was being hypocritical in claiming that "local voices matter" in his decision to block offshore drilling in ' + 
-          'Florida but not Virginia. Kaine asked: "Is it because the governor of Florida is a Republican and the Virginia governor is a Democrat?'
-}, {
-    id: '41db2ccc-944a-31a4-90e1-06b62cde7f46',
-    imageUrl: 'https://media.zenfs.com/en_US/News/TheWrap/Oprah_Would_Beat_Donald_Trump-b3cb921d9d68a116e0605084bba26f97',
+    imageUrl: 'test url',
     event: 'TRUMP ON TRAVEL BAN',
     provider: 'AOL.com',
     publishedDate: 3123123123123,
